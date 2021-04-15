5cc4270a7a4215b6692e94861524ccd541efd07f
Tomi E. Salami <salami.tomie@gmail.com>
2021-04-15 11:22:31 -0400

    updated page header, added code to display items in stock

--- admin/index.php
+++ admin/index.php
@@ -14,13 +14,47 @@ if(!isset($_SESSION['email']) & empty($_SESSION['email'])){
 			<div class="container">
 				<div class="row">
 					<div class="page_header text-center">
-						<h2>Shop</h2>
-						<p>You can order products from here</p>
+						<h2>Inventory</h2>
+						<!-- <p>You can order products from here</p> -->
 					</div>
 					<div class="col-md-12">
 						<div class="row">
 							<div id="shop-mason" class="shop-mason-4col">
+								<?php 
+                                $sql = "SELECT * FROM products";
+                                if (isset($_GET['id']) & !empty($_GET['id'])) {
+                                	$id = $_GET['id'];
+                                	$sql .= " WHERE catid=$id";
+                                }
+ 
+                                $res = mysqli_query($connection, $sql);
+                                while($r = mysqli_fetch_assoc($res)){
+                            ?>
 								<div class="sm-item isotope-item">
+									<div class="product">
+										<div class="product-thumb">
+											<img src="<?php echo $r['thumb']; ?>" class="img-responsive" alt="">
+											<!-- <div class="product-overlay">
+												<span>
+												<a href="single.php?id=<?php echo $r['id']; ?>" class="fa fa-link"></a>
+												<a href="#" class="fa fa-shopping-cart"></a>
+												</span>					
+											</div> -->
+										</div>
+										<!-- <div class="rating">
+											<span class="fa fa-star act"></span>
+											<span class="fa fa-star act"></span>
+											<span class="fa fa-star act"></span>
+											<span class="fa fa-star act"></span>
+											<span class="fa fa-star act"></span>
+										</div> -->
+										<h2 class="product-title"><a href="single.php?id=<?php echo $r['id']; ?>"><?php echo $r['name']; ?></a></h2>
+										<div class="product-price">CAD <?php echo $r['price']; ?>.00<span></span></div>
+									</div>
+								</div>
+								
+                            <?php } ?>
+								<!-- <div class="sm-item isotope-item">
 									<div class="product">
 										<div class="product-thumb">
 											<img src="images/shop/1.jpg" class="img-responsive" alt="">
@@ -41,7 +75,7 @@ if(!isset($_SESSION['email']) & empty($_SESSION['email'])){
 										<h2 class="product-title"><a href="#">Shave Knives</a></h2>
 										<div class="product-price">$79.00<span>$200.00</span></div>
 									</div>
-								</div>
+								</div> -->
 								
 							</div>
 						</div>
